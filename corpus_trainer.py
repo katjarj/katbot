a01f00cd52c4d329c3ecbad50af8b6a886edc5fc
Katja Radovic-Jonsson <169094310+katjarj@users.noreply.github.com>
2024-06-10 14:05:27 -0700

    Trains bot on the custom corpus

--- corpus_trainer.py
+++ corpus_trainer.py
@@ -2,4 +2,16 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from bot import chatbot
 
 trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
+trainer.train(
+    './kat_corpus/ai.yml',
+    './kat_corpus/botprofile.yml',
+    './kat_corpus/conversations.yml',
+    './kat_corpus/emotion.yml',
+    './kat_corpus/food.yml',
+    './kat_corpus/gossip.yml',
+    './kat_corpus/greetings.yml',
+    './kat_corpus/humor.yml',
+    './kat_corpus/psychology.yml',
+    './kat_corpus/science.yml',
+    './kat_corpus/sports.yml',
+)
